b4e000c447ac61b4e78c3f4d66ba00c27202d3e7
prontoTheLegend <anasprog8@gmail.com>
2025-04-26 16:18:17 +0300

    Update CV

--- src/store/Modules/Glubal/Store/state.js
+++ src/store/Modules/Glubal/Store/state.js
@@ -1,5 +1,5 @@
 export default function (){
     return {
-        cvUrl: 'https://drive.google.com/uc?export=download&id=1H6atQ_maaK4DALJfOG2ubK8BtsehsVAw',
+        cvUrl: 'https://drive.google.com/uc?export=download&id=1r7HY8BeIcJiIW0hY9gS8hCQejPTF3gbm',
     };
 };
