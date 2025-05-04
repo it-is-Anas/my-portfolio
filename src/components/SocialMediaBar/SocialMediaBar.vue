13184c4091bf11857214b790163b069da13edf2d
prontoTheLegend <anasprog8@gmail.com>
2025-05-04 23:24:14 +0300

    update contact bar

--- src/components/SocialMediaBar/SocialMediaBar.vue
+++ src/components/SocialMediaBar/SocialMediaBar.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="social-media-bar" >
-        <SocialMediaIcon :imgName="'fb.png'" :link="'https://www.facebook.com/profile.php?id=61574223360706'" />
-        <!-- <SocialMediaIcon :imgName="'li.png'" /> -->
-        <SocialMediaIcon :imgName="'in.png'"  :link="'https://www.instagram.com/its.anas777/?__pwa=1'"  />
+        <SocialMediaIcon :imgName="'fb.png'" :link="'https://www.facebook.com/share/18cwqbvyBr/'" />
+        <SocialMediaIcon :imgName="'li.png'" link="https://www.linkedin.com/in/anas-al-hourani-70a129364?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" />
+        <SocialMediaIcon :imgName="'in.png'"  :link="'https://www.instagram.com/it_is_anas7?igsh=MTJlcDZpbHcyd2cwbQ=='"  />
         <SocialMediaIcon :imgName="'github.png'" :link="'https://github.com/it-is-Anas'" />
     </div>
-</template>
+</template> 
 <script>
     import SocialMediaIcon from './SocialMediaIcon.vue';
     export default {
